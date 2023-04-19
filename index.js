c681f9313a732cfa758061bd0dd126ac5e47795c
Azizbek <azizbekturabayev@gmail.com>
2023-04-19 18:22:17 +0500

    o'zgarish

---
+++ index.js
@@ -0,0 +1,22 @@
+function myFun1(){
+    document.getElementById("img1").style.display="block";
+    document.getElementById("img2").style.display="none";
+    document.getElementById("img3").style.display="none";
+
+
+}
+
+
+function myFun2(){
+    document.getElementById("img2").style.display="block";
+    document.getElementById("img2").style.display="border[solid #91916C 5px ]";
+    document.getElementById("img1").style.display="none";
+    document.getElementById("img3").style.display="none";
+}
+
+
+function myFun3(){
+    document.getElementById("img3").style.display="block";      
+    document.getElementById("img1").style.display="none";
+    document.getElementById("img2").style.display="none";
+}
