da1c92388b15b47cc461a3aa9f434500b4c09a53
Azizbek <azizbekturabayev@gmail.com>
2023-04-19 18:28:03 +0500

    o'zgarishlar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      display: ["group-hover"],
+    },
+    colors: {
+      'color1': '#91916C',
+      'color2': '#E5A985',
+      
+      
+    }
+  },
+  plugins: [
+  
+  ],
+}
