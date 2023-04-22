ccdd9d22d8003bdc3271c52308fdf96cf5eee73f
Azizbek <azizbekturabayev@gmail.com>
2023-04-22 19:53:27 +0500

    slider

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function myFun1(){
 
 function myFun2(){
     document.getElementById("img2").style.display="block";
-    document.getElementById("img2").style.display="border[solid #91916C 5px ]";
+    document.getElementById("img2").style.display="background-[#0000]";
     document.getElementById("img1").style.display="none";
     document.getElementById("img3").style.display="none";
 }
@@ -17,6 +17,7 @@ function myFun2(){
 
 function myFun3(){
     document.getElementById("img3").style.display="block";      
+    document.getElementById("img3").style.display="background-[#0000]";      
     document.getElementById("img1").style.display="none";
     document.getElementById("img2").style.display="none";
 }
